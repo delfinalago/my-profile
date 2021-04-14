07937994dfee0d49dddb55e88aced03db7244b11
delfinalago <delfinalago@gmail.com>
2021-04-14 14:16:27 -0300

    deploy my profile to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import delfiProfile from './img/delfiProfile.png'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My profile deployed!</h1>
+        <h2>I can't believe is working</h2>
+        <img src={delfiProfile} alt="profile-imagen" />
     </div>
   );
 }
